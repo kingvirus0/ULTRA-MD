88d36c3e8339bd23abd6069025b83efe9e230bb7
kingvirus0 <151681163+kingvirus0@users.noreply.github.com>
2024-11-30 10:00:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 
-global.canal = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.canal = 'https://whatsapphannel/0029VagJIAr3bbVBCpEkAM07'
 
 
 global.APIs = {
@@ -74,13 +74,13 @@ global.APIKeys = {
 // Sticker WM
 global.premium = 'true'
 global.packname = 'GLOBAL-MD'
-global.author = 'GlobalTechInfo'
+global.author = 'kingvirus '
 global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
-global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/global.techinfo'
-global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
-global.fgsc = 'https://github.com/GlobalTechInfo/ULTRA-MD'
-global.fgyt = 'https://youtube.com/@GlobalTechInfo'
-global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
+global.igfg = ''
+global.dygp = ''
+global.fgsc = 'https:/lobalTechInfo/ULTRA-MD'
+global.fgyt = 'https://youtu.alTechInfo'
+global.fgpyp = 'https://youtubebalTechInfo'
 global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
 global.thumb = fs.readFileSync('./assets/qasim.jpg')
 
